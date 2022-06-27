c90df8201776c58fdaf485bcd4f0d282f76ffb8e
Timothy Smith <timsmith204@utexas.edu>
2022-06-26 20:31:04 -0500

    atlantic sections of llc

--- src/llcutils.py
+++ src/llcutils.py
@@ -2,6 +2,7 @@ import numpy as np
 import xarray as xr
 
 def get_pacific(xds):
+
     xds = xds.isel(face=slice(7,None))
     mlist = []
 
@@ -37,3 +38,51 @@ def get_pacific(xds):
                        'standard_name': 'y_index'
                       }
     return pac
+
+def get_atlantic(xds):
+
+    xds = xds.sel(face=[0, 1, 2, 10, 11, 12])
+
+def get_atlantic(xds):
+
+    xds = xds.sel(face=[0, 1, 2, 10, 11, 12])
+
+    xlist = []
+    for face in [0, 1, 2]:
+
+        xx = xds.sel(face=face)
+        jnew = np.arange(90) + face*90
+        xx['j_new'] = xr.DataArray(jnew, coords=xds.j.coords, dims=xds.j.dims)
+        xx = xx.swap_dims({'j':'j_new'}).drop('j').rename({'j_new':'iy'})
+
+        inew = np.arange(90, 180)
+        xx['i_new'] = xr.DataArray(inew, coords=xds.i.coords, dims=xds.i.dims)
+        xx = xx.swap_dims({'i':'i_new'}).drop('i').rename({'i_new':'ix'})
+
+        xlist.append(xx)
+
+    atl1 = xr.concat(xlist, dim='iy')
+
+    xlist = []
+    for face in [10,11,12]:
+
+        xx = xds.sel(face=face)
+        i = 12-face
+        inew = np.arange(89,-1,-1)+i*90
+        xx['i_new'] = xr.DataArray(inew, xds.i.coords, xds.i.dims)
+        xx = xx.swap_dims({'i':'i_new'}).drop('i').rename({'i_new':'iy'})
+
+        jnew = np.arange(90)
+        xx['j_new'] = xr.DataArray(jnew, xds.j.coords, xds.j.dims)
+        xx = xx.swap_dims({'j':'j_new'}).drop('j').rename({'j_new':'ix'})
+        xlist.append(xx)
+
+    atl2 = xr.concat(xlist, dim='iy')
+    atl = xr.concat([atl2, atl1], dim='ix')
+    atl['ix'].attrs = {'long_name':'Real x direction',
+                       'standard_name': 'x_index'
+                      }
+    atl['iy'].attrs = {'long_name':'Real y direction',
+                       'standard_name': 'y_index'
+                      }
+    return atl
