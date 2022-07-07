bf4419e54b32508528c2ec1d78db2f08eaec23d0
Timothy Smith <timsmith204@utexas.edu>
2022-07-06 21:38:28 -0500

    sample with arbitrary num applications

---
+++ src/sample_llc90_napps.py
@@ -0,0 +1,41 @@
+"""Run 3D LLC sampling"""
+
+from xmitgcm import open_mdsdataset
+
+from sampledriver import SampleDriver
+
+
+if __name__ == "__main__":
+
+    main_run='/scratch2/tsmith/generic-matern-covariance/sampling/llc90'
+    ds = open_mdsdataset(f"{main_run}/grid", iters=None, geometry='llc')
+
+    # --- directories and dicts
+    dirs = {'main_run'      : main_run,
+            'netcdf'        : main_run+'/ncfiles'}
+
+    dsim = {'machine'       : 'sverdrup',
+            'n_procs'       : 96,
+            'exe_file'      : main_run+'/build/mitgcmuv',
+            'binary_dir'    : main_run+'/bin',
+            'namelist_dir'  : main_run+'/input',
+            'time'          : '72:00:00'}
+
+    slurm = {'be_nice':True,'max_job_submissions':9,'dependency':'afterany'}
+
+    # --- Launch
+    log10tol = -3
+    for n_apps in [2, 3, 4]:
+        driver = SampleDriver(f'matern-{n_apps:02d}/log10tol{log10tol:03d}-3D')
+        driver.start(dirs=dirs,
+                     dsim=dsim,
+                     mymodel=ds['maskC'],
+                     ctrl_ds=ds,
+                     NxList=[5, 10, 15, 20],
+                     xiList=[1],
+                     sorDict={.5:1.6, 1:1.3, 2:1.06},
+                     slurm=slurm,
+                     n_samples=1000,
+                     smooth2DDims=None,
+                     elliptic_tol=10**log10tol,
+                     num_applications=n_apps)
